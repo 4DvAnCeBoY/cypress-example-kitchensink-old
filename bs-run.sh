4c7fc8de33880106bde18410bf7706696ada5e45
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2020-11-02 16:43:55 +0530

    Update bs-run.sh

--- bs-run.sh
+++ bs-run.sh
@@ -3,4 +3,5 @@
 
 npm install -g browserstack-cypress-cli
 browserstack-cypress init
+BrowserStackLocal --key CsbtfqVzuuBShxEgq1K3 --daemon start
 browserstack-cypress run --username mohammadasadkhan1 --key CsbtfqVzuuBShxEgq1K3
