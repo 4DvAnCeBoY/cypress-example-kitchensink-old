3ee410d1ca6754773a572aa9ed4aa634f438b079
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2022-04-25 22:13:32 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 echo "Executing Test"
 lambdatest-cypress run sync=true 
 echo "Downloading Assets"
-lambdatest-cypress generate-report --user=$LT_USERNAME --access_key=$LT_ACCESS_KEY --sid=$SESSION_ID
+lambdatest-cypress generate-report
 echo "Merging Files"
 find ./lambdatest-artefacts -type f -name "*.json" -exec  cat {} \; > output.json 
